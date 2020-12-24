6c8d2a5357f62cba3a71069b5bc979e9af0d1e54
Tom Eldon <60324543+Wumfie@users.noreply.github.com>
2020-12-24 18:56:12 +0000

    Removed update_interval and fixed indentation

--- custom_components/hpma115s0_esphome/hpma115s0_esphome.h
+++ custom_components/hpma115s0_esphome/hpma115s0_esphome.h
@@ -10,35 +10,33 @@
 //HardwareSerial hpmaSerial(9600, SERIAL_8N1, 10, 9);
 
 namespace esphome {
-namespace hpma115s0_esphome {
+  namespace hpma115s0_esphome {
 
-class hpma115S0CustomSensor : public PollingComponent, public uart::UARTDevice {
- public:
-  hpma115S0CustomSensor() = default;
+    class hpma115S0CustomSensor : public PollingComponent, public uart::UARTDevice {
+    public:
+      hpma115S0CustomSensor() = default;
 
-  void set_pm_2_5_sensor(sensor::Sensor *pm_2_5_sensor) { pm_2_5_sensor_ = pm_2_5_sensor; }
-  void set_pm_10_0_sensor(sensor::Sensor *pm_10_0_sensor) { pm_10_0_sensor_ = pm_10_0_sensor; }
-  
-  void setup() override;
-  void dump_config() override;
-  void loop() override;
-  void update() override;
-  float get_setup_priority() const override;
+      void set_pm_2_5_sensor(sensor::Sensor *pm_2_5_sensor) { pm_2_5_sensor_ = pm_2_5_sensor; }
+      void set_pm_10_0_sensor(sensor::Sensor *pm_10_0_sensor) { pm_10_0_sensor_ = pm_10_0_sensor; }
+      
+      void setup() override;
+      void dump_config() override;
+      void loop() override;
+      void update() override;
+      float get_setup_priority() const override;
 
-  int comAvailableRead(int availableMustBeLowerThan);
+      int comAvailableRead(int availableMustBeLowerThan);
 
-  bool read_values(float *p25, float *p10);
-	bool stop_measurement();
-	bool start_measurement();
-	bool stop_autosend();
-	bool enable_autosend();
+      bool read_values(float *p25, float *p10);
+      bool stop_measurement();
+      bool start_measurement();
+      bool stop_autosend();
+      bool enable_autosend();
 
- protected:
-  sensor::Sensor *pm_2_5_sensor_{nullptr};
-  sensor::Sensor *pm_10_0_sensor_{nullptr};
+    protected:
+      sensor::Sensor *pm_2_5_sensor_{nullptr};
+      sensor::Sensor *pm_10_0_sensor_{nullptr};
+    };
 
-  uint32_t update_interval_ = 3000;
-};
-
-}
+  }
 }
